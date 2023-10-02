8c64dbc2c77920385a869bd30fd62f70710a86b2
Ilia Perfilov <ind.climb@gmail.com>
2023-10-02 18:33:26 +0300

    API views for looking and creating were added

--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from robots.models import Robot
+from robots.serializers import RobotSerializer
+
+
+class RobotCreateAPIView(generics.CreateAPIView):
+    serializer_class = RobotSerializer
+
+
+class RobotListAPIView(generics.ListAPIView):
+    serializer_class = RobotSerializer
+    queryset = Robot.objects.all()
