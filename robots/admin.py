d20f05b09b04cbfca55b0a4bb11e6d1c2ef0ad97
Ilia Perfilov <ind.climb@gmail.com>
2023-10-02 17:17:30 +0300

    app 'robots' was added

---
+++ robots/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from robots.models import Robot
+
+
+@admin.register(Robot)
+class RobotAdmin(admin.ModelAdmin):
+    list_display = ('id', 'serial', 'model', 'version', 'created',)
