9e57ff053ca953fb0b4d22e718f1b2d01f0ec078
Ilia Perfilov <ind.climb@gmail.com>
2023-10-05 14:33:53 +0300

    func 'send_mail_to_customers' was created

---
+++ customers/services.py
@@ -0,0 +1,14 @@
+from django.core.mail import send_mail
+
+from R4C import settings
+
+
+def send_mail_to_customers(subject, body, customers_list):
+    send_mail(
+        subject=subject,
+        message=body,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=customers_list
+    )
+
+
