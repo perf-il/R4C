1249c24307e4f4d92ad43208e159573f7e453c9b
Ilia Perfilov <ind.climb@gmail.com>
2023-10-05 14:39:04 +0300

    'OrderCreateAPIView' and 'OrderListAPIView' were added

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from orders.models import Order
+from orders.serializers import OrderSerializer
+from robots.models import Robot
+
+
+class OrderCreateAPIView(generics.CreateAPIView):
+    serializer_class = OrderSerializer
+
+    def perform_create(self, serializer):
+        new_order = serializer.save()
+        new_order.robot_serial = new_order.robot_serial.upper()
+        new_order.in_stock = Robot.objects.filter(serial=new_order.robot_serial).exists()
+        new_order.save()
+
+
+class OrderListAPIView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
