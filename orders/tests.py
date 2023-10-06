12db45e5c32f963340fa67d1e515d691e671e5b6
Ilia Perfilov <ind.climb@gmail.com>
2023-10-06 18:05:04 +0300

    unit tests for 'orders' were added

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from customers.models import Customer
+from orders.models import Order
+
+
+class OrdersTest(APITestCase):
+
+    def setUp(self) -> None:
+        self.customer = Customer.objects.create(
+            email='test@example.com',
+        )
+
+    def test_create_order(self):
+        """Тестирование создания нового заказа"""
+        data = {
+            'customer': self.customer.pk,
+            'robot_serial': 'R2-D2'
+        }
+
+        response = self.client.post(
+            reverse('orders:order-create'),
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+        self.assertTrue(Order.objects.all().exists())
+
+        self.assertEqual(
+                response.json(),
+                {
+                    'id': 1,
+                    'customer': self.customer.pk,
+                    'robot_serial': 'R2-D2',
+                    'in_stock': False
+                }
+            )
+
